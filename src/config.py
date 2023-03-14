0179e2ab518d8e006e42cd6114fcac179ffc28ce
glebfel <felyust@list.ru>
2023-03-15 02:43:43 +0300

    add config module

---
+++ src/config.py
@@ -0,0 +1,31 @@
+import pathlib
+from dotenv import load_dotenv
+from pydantic import BaseSettings, Field
+
+# root directory
+ROOT_PATH = str(pathlib.Path(__file__).parent.parent.parent)
+
+
+class Settings(BaseSettings):
+    # for main db
+    POSTGRES_USER: str = Field(..., env="POSTGRES_USER")
+    POSTGRES_PASSWORD: str = Field(..., env="POSTGRES_PASSWORD")
+    POSTGRES_HOST: str = Field(..., env="POSTGRES_HOST")
+    POSTGRES_PORT: str = Field(..., env="POSTGRES_PORT")
+    POSTGRES_DB: str = Field(..., env="POSTGRES_DB")
+    # for api
+    FAST_API_HOST: str = Field(..., env="FAST_API_HOST")
+    FAST_API_PORT: int = Field(..., env="FAST_API_PORT")
+
+    class Config:
+        env_prefix = ""
+        case_sentive = False
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+# load env from file
+load_dotenv()
+
+# load vars to settings
+settings = Settings()
