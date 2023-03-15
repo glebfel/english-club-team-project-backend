daf6706db32b4bc6c79486a1f354194559d7071d
glebfel <felyust@list.ru>
2023-03-16 02:07:26 +0300

    fix bugs in auth process

--- src/auth/router.py
+++ src/auth/router.py
@@ -2,10 +2,10 @@ from datetime import datetime, timedelta
 
 import jwt
 from fastapi import HTTPException, Depends, APIRouter, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 
-from src.auth.schemas import Token
+from src.auth.schemas import Token, UserRegister, UserLogin, UserInfo
 from src.config import settings
 from src.db.crud.users import get_user_by_email, add_new_user
 from src.db.models import User
@@ -17,17 +17,17 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 router = APIRouter(tags=["auth"], prefix='/auth')
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserInfo:
     try:
         payload = jwt.decode(token, settings.AUTH_SECRET_KEY, algorithms=[settings.ALGORITHM])
-        email: str = payload.get("sub")
+        email: str = payload.get("email")
         if email is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Could not validate credentials",
                 headers={"WWW-Authenticate": "Bearer"},
             )
-        token_data = {"sub": email}
+        expire: datetime = payload.get("exp")
     except jwt.PyJWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -37,7 +37,8 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     user = get_user_by_email(email)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return user
+    return UserInfo(name=user.name, email=user.email,
+                    is_admin=user.is_admin, access_token=Token(access_token=token, expire=expire, token_type='Bearer'))
 
 
 def verify_password(plain_password, hashed_password) -> bool:
@@ -48,12 +49,12 @@ def get_password_hash(password) -> str:
     return pwd_context.hash(password)
 
 
-def authenticate_user(email: str, password: str):
+def authenticate_user(email: str, password: str) -> User | None:
     user = get_user_by_email(email)
     if not user:
-        return False
+        return None
     if not verify_password(password, user.hashed_password):
-        return False
+        return None
     return user
 
 
@@ -66,33 +67,34 @@ def create_access_token(data: dict, expires_delta: timedelta) -> str:
 
 
 @router.post("/register")
-def register_user(user: User) -> Token:
+def register_user(user: UserRegister) -> Token:
     db_user = get_user_by_email(email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    user.hashed_password = get_password_hash(user.password)
-    add_new_user(user=user)
+    add_new_user(User(name=user.name, email=user.email, hashed_password=get_password_hash(user.password)))
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": db_user.email, "name": db_user.name, "is_admin": db_user.is_admin},
+        data={"email": db_user.email, "name": db_user.name,
+              "is_admin": db_user.is_admin, "exp": datetime.utcnow() + access_token_expires},
         expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type='bearer', expire=datetime.utcnow() + access_token_expires)
 
 
 @router.post("/login")
-def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
-    user = authenticate_user(form_data.username, form_data.password)
+def login_for_access_token(user: UserLogin) -> Token:
+    user = authenticate_user(user.email, user.password)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect email or password")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.email, "name": user.name, "is_admin": user.is_admin},
+        data={"email": user.email, "name": user.name,
+              "is_admin": user.is_admin, "exp": datetime.utcnow() + access_token_expires},
         expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type='bearer', expire=datetime.utcnow() + access_token_expires)
 
 
 @router.get("/me")
-def read_users_me(current_user: User = Depends(get_current_user)):
+def read_users_me(current_user: User = Depends(get_current_user)) -> UserInfo:
     return current_user
--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,9 +1,28 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
     expire: datetime
+
+
+class UserRegister(BaseModel):
+    name: str = Field(max_length=30)
+    email: EmailStr
+    password: str
+    is_admin: bool = Field(default=False)
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserInfo(BaseModel):
+    name: str = Field(max_length=30)
+    email: EmailStr
+    is_admin: bool = Field(default=False)
+    access_token: Token
