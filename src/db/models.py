38d4e80bc7aafea7ad93656397748aa4be2d7b93
glebfel <felyust@list.ru>
2023-03-16 13:04:18 +0300

    change User db model (add unique constraint to email field)

--- src/db/models.py
+++ src/db/models.py
@@ -10,7 +10,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String, index=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
     name = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_admin = Column(Boolean, default=False)
