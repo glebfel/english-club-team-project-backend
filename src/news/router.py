4df2a044394853dee7584ceb96120fcf4cbcdf58
glebfel <felyust@list.ru>
2023-03-16 13:23:12 +0300

    update news module dependencies

--- src/news/router.py
+++ src/news/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 
-from src.auth.dependencies import oauth2_scheme
+from src.auth.dependencies import get_current_user
 from src.db.crud.news import get_news as get_news_db, add_news as add_news_db
 from src.news.dependencies import check_user_status
 from src.news.schemas import NewsList, NewsItem
@@ -8,7 +8,7 @@ from src.news.schemas import NewsList, NewsItem
 router = APIRouter(tags=["news"], prefix='/news')
 
 
-@router.get("/all", dependencies=[Depends(oauth2_scheme)])
+@router.get("/all", dependencies=[Depends(get_current_user)])
 def get_all_news() -> NewsList:
     return NewsList(news=[NewsItem(title=i.title, content=i.content) for i in get_news_db()])
 
