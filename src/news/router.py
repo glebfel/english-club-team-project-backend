2d25c688dc9438517923a6e46b006bc5b9d41020
glebfel <felyust@list.ru>
2023-03-16 17:33:01 +0300

    add get_news_info_by_id to news module

--- src/news/router.py
+++ src/news/router.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from src.auth.dependencies import get_current_user, check_user_status
-from src.db.crud.news import get_news as get_news_db, add_news as add_news_db
+from src.db.crud.news import get_news as get_news_db, add_news as add_news_db, get_news_by_id
 from src.news.schemas import NewsList, NewsItem
 
 news_router = APIRouter(tags=["News"], prefix='/news')
@@ -17,3 +17,11 @@ def get_all_news() -> NewsList:
 def add_news(news: NewsItem):
     """Add new news (required admin permission)"""
     add_news_db(news.title, news.content)
+
+
+@news_router.get("/info", dependencies=[Depends(get_current_user)])
+def get_news_info_by_id(news_id: int) -> NewsItem:
+    """Get news info"""
+    if not (news := get_news_by_id(news_id)):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="News not found")
+    return NewsItem(**news.dict())
--- src/news/schemas.py
+++ src/news/schemas.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class NewsItem(BaseModel):
+    id: int
     title: str
     content: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
