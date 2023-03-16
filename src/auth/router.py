2e57c538ec9f7430367b2399d554517acc7fc9e1
glebfel <felyust@list.ru>
2023-03-16 18:43:51 +0300

    fix auth router

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
 
 import jwt
-from fastapi import HTTPException, Depends, APIRouter
+from fastapi import HTTPException, Depends, APIRouter, status
+from fastapi.security import OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 
-from src.auth.schemas import Token, UserRegister, UserLogin
+from src.auth.schemas import Token, UserRegister
 from src.config import settings
 from src.db.crud.users import get_user_by_phone_number, add_new_user
 from src.db.models import User
@@ -43,15 +44,14 @@ def create_access_token(data: dict, expires_delta: timedelta) -> str:
 def register_user(user: UserRegister) -> Token:
     db_user = get_user_by_phone_number(phone_number=user.phone_number)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Phone number already registered")
     add_new_user(first_name=user.first_name, last_name=user.last_name,
                  email=user.email, username=user.username,
                  phone_number=user.phone_number, birthday=user.birthday,
                  hashed_password=get_password_hash(user.password), admin=user.is_admin)
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"first_name": user.first_name, "last_name": user.last_name, "username": user.username,
-              "phone_number": user.phone_number, "email": user.email, "birthday": user.birthday,
+        data={"username": user.username, "phone_number": user.phone_number, "email": user.email,
               "is_admin": user.is_admin, "exp": datetime.utcnow() + access_token_expires},
         expires_delta=access_token_expires
     )
@@ -59,13 +59,13 @@ def register_user(user: UserRegister) -> Token:
 
 
 @auth_router.post("/login")
-def login_for_access_token(form_data: UserLogin = Depends()) -> Token:
-    user = authenticate_user(form_data.phone_number, form_data.password)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
+    user = authenticate_user(form_data.username, form_data.password)
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect phone number or password")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect phone number or password")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"email": user.email, "name": user.name,
+        data={"username": user.username, "phone_number": user.phone_number, "email": user.email,
               "is_admin": user.is_admin, "exp": datetime.utcnow() + access_token_expires},
         expires_delta=access_token_expires
     )
