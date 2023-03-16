7046699725e3e529441176f3bedc0fb00e4c3c2d
glebfel <felyust@list.ru>
2023-03-16 15:58:26 +0300

    update UserInfo model

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, HttpUrl
+
+from shifts.schemas import Shift
 
 
 class Token(BaseModel):
@@ -10,19 +12,32 @@ class Token(BaseModel):
 
 
 class UserRegister(BaseModel):
-    name: str = Field(max_length=30)
+    first_name: str = Field(max_length=30)
+    last_name: str = Field(max_length=30)
+    username: str = Field(max_length=30)
+    birthday: datetime
+    phone_number: str
     email: EmailStr
     password: str
     is_admin: bool = Field(default=False)
 
 
 class UserLogin(BaseModel):
-    email: EmailStr
+    phone_number: str
     password: str
 
 
 class UserInfo(BaseModel):
-    name: str = Field(max_length=30)
+    first_name: str = Field(max_length=30)
+    last_name: str = Field(max_length=30)
+    username: str = Field(max_length=30)
+    birthday: datetime
+    phone_number: str
     email: EmailStr
+    first_shift: Shift = Field(default=None)
+    experience: int = Field(default=None)
+    rank: str = Field(default=None)
+    hobby: str = Field(max_length=30)
+    media_link: HttpUrl = Field(default=None)
     is_admin: bool = Field(default=False)
     access_token: Token
