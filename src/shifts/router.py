e60dee0995e8f6c1abcbb4b3f428f00f4d6bec83
glebfel <felyust@list.ru>
2023-03-16 13:48:08 +0300

    modify shifts module endpoints

--- src/shifts/router.py
+++ src/shifts/router.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Depends
+from datetime import datetime
+
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from src.auth.dependencies import get_current_user
+from src.db.crud.shifts import get_all_shifts, get_shift_by_id, add_shift as add_shift_db
 from src.news.dependencies import check_user_status
 from src.shifts.schemas import Shift
 
@@ -9,14 +12,16 @@ router = APIRouter(tags=["shifts"], prefix='/shifts')
 
 @router.get("/upcoming", dependencies=[Depends(get_current_user)])
 def get_upcoming_shifts() -> list[Shift]:
-    return []
+    return [shift for shift in get_all_shifts() if shift.start_date > datetime.now()]
 
 
 @router.get("/info", dependencies=[Depends(get_current_user)])
-def get_shift_info() -> list[Shift]:
-    return []
+def get_shift_info(shift_id: int) -> Shift | None:
+    if not (shift := get_shift_by_id(shift_id)):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Shift not found")
+    return Shift(**shift.dict())
 
 
 @router.post("/add", dependencies=[Depends(check_user_status)])
 def add_shift(shift: Shift):
-    pass
+    add_shift_db(shift.name, shift.start_date, shift.end_date)
