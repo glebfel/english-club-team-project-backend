8c95387d866b72f44b558a32e6976be6002b6861
glebfel <felyust@list.ru>
2023-03-26 14:42:12 +0300

    update docker files

--- src/config.py
+++ src/config.py
@@ -26,7 +26,7 @@ class Settings(BaseSettings):
     class Config:
         env_prefix = ""
         case_sentive = False
-        env_file = '.env'
+        env_file = '../.env'
         env_file_encoding = 'utf-8'
 
 
