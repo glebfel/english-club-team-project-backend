c24a9853f0c9312dd710128336ad3f2698911026
glebfel <felyust@list.ru>
2023-03-26 12:36:34 +0300

    fix bugs in news models

--- src/news/router.py
+++ src/news/router.py
@@ -2,28 +2,28 @@ from fastapi import APIRouter, Depends, HTTPException, status
 
 from auth.dependencies import get_current_user, check_user_status
 from db.crud.news import get_news as get_news_db, add_news as add_news_db, get_news_by_id
-from news.schemas import NewsItem
+from news.schemas import NewsInfo, NewsBase
 from utils import convert_sqlalchemy_row_to_dict
 
 news_router = APIRouter(tags=["News"], prefix='/news')
 
 
 @news_router.get("/all", dependencies=[Depends(get_current_user)])
-def get_all_news() -> list[NewsItem]:
+def get_all_news() -> list[NewsInfo]:
     """Get all news"""
-    return [NewsItem(id=i.id, title=i.title, content=i.content, created_at=i.created_at) for i in get_news_db()]
+    return [NewsInfo(id=i.id, title=i.title, content=i.content, created_at=i.created_at) for i in get_news_db()]
 
 
 @news_router.post("/add", dependencies=[Depends(check_user_status)])
-def add_news(news: NewsItem):
+def add_news(news: NewsBase):
     """Add new news (required admin rights)"""
     add_news_db(news.title, news.content)
     return {'status': 'success', 'message': 'News added'}
 
 
 @news_router.get("/info/{news_id}", dependencies=[Depends(get_current_user)])
-def get_news_info_by_id(news_id: int) -> NewsItem:
+def get_news_info_by_id(news_id: int) -> NewsInfo:
     """Get news info"""
     if not (news := get_news_by_id(news_id)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="News not found")
-    return NewsItem(**convert_sqlalchemy_row_to_dict(news))
+    return NewsInfo(**convert_sqlalchemy_row_to_dict(news))
--- src/news/schemas.py
+++ src/news/schemas.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
-class NewsItem(BaseModel):
-    id: int
+class NewsBase(BaseModel):
     title: str
     content: str
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+
+class NewsInfo(NewsBase):
+    id: int
+    created_at: datetime
